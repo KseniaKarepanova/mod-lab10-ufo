7bcdff508c0e9df066cfe354b45e63d90f91902e
kseniaserkar@gmail.com <kseniaserkar@gmail.com>
2023-05-28 22:48:01 +0300

    add files

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button_Chart = new System.Windows.Forms.Button();
+            this.button_Move = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(25, 1);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(751, 385);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // button_Chart
+            // 
+            this.button_Chart.Location = new System.Drawing.Point(166, 397);
+            this.button_Chart.Name = "button_Chart";
+            this.button_Chart.Size = new System.Drawing.Size(199, 35);
+            this.button_Chart.TabIndex = 1;
+            this.button_Chart.Text = "Chart";
+            this.button_Chart.UseVisualStyleBackColor = true;
+            this.button_Chart.Click += new System.EventHandler(this.button_Chart_Click);
+            // 
+            // button_Move
+            // 
+            this.button_Move.Location = new System.Drawing.Point(396, 398);
+            this.button_Move.Name = "button_Move";
+            this.button_Move.Size = new System.Drawing.Size(204, 34);
+            this.button_Move.TabIndex = 2;
+            this.button_Move.Text = "Move";
+            this.button_Move.UseVisualStyleBackColor = true;
+            this.button_Move.Click += new System.EventHandler(this.button_Move_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button_Move);
+            this.Controls.Add(this.button_Chart);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button button_Chart;
+        private System.Windows.Forms.Button button_Move;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        bool is_paint = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Chart_Click(object sender, EventArgs e)
+        {
+            chart1.Visible = true;
+            chart1.ChartAreas.Clear();
+            chart1.Series.Clear();
+            chart1.ChartAreas.Add("area1");
+            chart1.Series.Add("Dependence of accuracy on the radius of the hit zone");
+            chart1.Series["Dependence of accuracy on the radius of the hit zone"].ChartArea = "area1";
+            chart1.Series["Dependence of accuracy on the radius of the hit zone"].ChartType = SeriesChartType.Spline;
+            chart1.Series["Dependence of accuracy on the radius of the hit zone"].Color = Color.Blue;
+            chart1.ChartAreas[0].AxisX.Title = "Radius of the hit zone";
+            chart1.ChartAreas[0].AxisY.Title = "Accuracy";
+            chart1.ChartAreas[0].AxisX.Minimum = 0;
+
+            int step = 1;
+            int x1 = 1;
+            int y1 = 10;
+            Point p1 = new Point(x1, y1);
+            int x2 = 510;
+            int y2 = 530;
+            Point p2 = new Point(x2, y2);
+
+            for (double i = 1; i <= 101; i = i + 5)
+            {
+                int n = Count_n(p1.X, p1.Y, p2.X, p2.Y, step, i);
+                ChartForm(i, n);
+            }
+        }
+
+        private void button_Move_Click(object sender, EventArgs e)
+        {
+            chart1.Visible = false;
+            is_paint = true;
+            Invalidate();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            if (is_paint)
+            {
+                int step = 1;
+                int x1 = 1;
+                int y1 = 10;
+                int x2 = 110;
+                int y2 = 130;
+                double eps = 5;
+                int n = 5;
+                Paint_Points(x1, y1, x2, y2, n, step, eps, e.Graphics);
+            }
+        }
+
+        void ClearPoint(double X, double Y, Graphics g, double radius)
+        {
+            Pen pen = new Pen(Color.White);
+            g.DrawEllipse(pen, (float)X, (float)Y, (float)radius, (float)radius);
+        }
+        void DrawPoint(double X, double Y, Graphics g, double radius)
+        {
+            Pen pen = new Pen(Color.Black);
+            g.DrawEllipse(pen, (float)X, (float)Y, (float)radius, (float)radius);
+        }
+        void ChartForm(double x, double y)
+        {
+            chart1.Series["Dependence of accuracy on the radius of the hit zone"].Points.AddXY(x, y);
+
+        }
+
+        int Count_n(int x1, int y1, int x2, int y2, int step, double eps)
+        {
+            int n = 1;
+            bool is_finish = false;
+            double alpha;
+            while ((n < 50) && (is_finish == false))
+            {
+                int dx = Math.Abs(x2 - x1);
+                int dy = Math.Abs(y2 - y1);
+
+                double otn = (double)dy / dx;
+                if (otn <= 1)
+                {
+                    alpha = Atan(n, otn);
+                }
+                else
+                {
+                    otn = (double)dx / dy;
+                    alpha = Math.PI / 2 - Atan(n, otn);
+
+                }
+
+                double distance = Math.Sqrt(Math.Pow((x1 - x2), 2) + Math.Pow((y1 - y2), 2)); ;
+                double distance_last = Math.Sqrt(Math.Pow((x1 - x2), 2) + Math.Pow((y1 - y2), 2));
+                double x = x1;
+                double y = y1;
+
+                while (true)
+                {
+                    if (distance <= distance_last)
+                    {
+                        distance_last = Math.Sqrt(Math.Pow((x - x2), 2) + Math.Pow((y - y2), 2));
+                        x += step * Cos(n, alpha);
+                        y += step * Sin(n, alpha);
+                        distance = Math.Sqrt(Math.Pow((x - x2), 2) + Math.Pow((y - y2), 2));
+                        if (distance <= eps)
+                        {
+                            is_finish = true;
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        n++;
+                        break;
+                    }
+
+                }
+
+
+            }
+            return n;
+        }
+
+
+        void Paint_Points(int x1, int y1, int x2, int y2, int n, int step, double eps, Graphics g)
+        {
+            double x = x1;
+            double y = y1;
+            double alpha;
+            DrawPoint(x1, y1, g, eps);
+            DrawPoint(x2, y2, g, eps);
+
+            int dx = Math.Abs(x2 - x1);
+            int dy = Math.Abs(y2 - y1);
+
+            double otn = (double)dy / dx;
+            if (otn <= 1)
+            {
+                alpha = Atan(n, otn);
+            }
+            else
+            {
+                otn = (double)dx / dy;
+                alpha = Math.PI / 2 - Atan(n, otn);
+
+            }
+
+            double distance = Math.Sqrt(Math.Pow((x1 - x2), 2) + Math.Pow((y1 - y2), 2)); ;
+            double distance_last = Math.Sqrt(Math.Pow((x1 - x2), 2) + Math.Pow((y1 - y2), 2));
+
+
+            while (true)
+            {
+                if (distance <= distance_last)
+                {
+                    Thread.Sleep(50);
+                    ClearPoint(x, y, g, eps);
+                    distance_last = Math.Sqrt(Math.Pow((x - x2), 2) + Math.Pow((y - y2), 2));
+                    x += step * Cos(n, alpha);
+                    y += step * Sin(n, alpha);
+                    DrawPoint(x, y, g, eps);
+                    distance = Math.Sqrt(Math.Pow((x - x2), 2) + Math.Pow((y - y2), 2));
+                    if (distance <= eps)
+                    {
+                        break;
+                    }
+                }
+                else
+                {
+                    break;
+                }
+
+            }
+        }
+
+
+        double Atan(int n, double x)
+        {
+            double result = 0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                result = result + Math.Pow(-1, (i - 1)) * Math.Pow(x, (2 * i - 1)) / (2 * i - 1);
+            }
+
+            return result;
+        }
+
+
+
+        double Sin(int n, double x)
+        {
+            double result = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                result = result + Math.Pow(-1, (i - 1)) * Math.Pow(x, (2 * i - 1)) / Factorial(2 * i - 1);
+            }
+            return result;
+        }
+
+        double Cos(int n, double x)
+        {
+            double result = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                result = result + Math.Pow(-1, (i - 1)) * Math.Pow(x, (2 * i - 2)) / Factorial(2 * i - 2);
+            }
+            return result;
+        }
+
+        double Factorial(double n)
+        {
+            double factorial = 1;
+            for (int i = 1; i <= n; i++)
+                factorial = factorial * i;
+            return factorial;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            chart1.Visible = false;
+        }
+    }
+}
